5814281b85d96545bcb9ba7233b63a62ab920449
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-05-27 17:47:27 +0930

    debug login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
         {data ? (
               <p>
                 Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
+                <Link to="/dashboard">back to the homepage.</Link>
               </p>
             ) : (
         <form onSubmit={handleFormSubmit}>
